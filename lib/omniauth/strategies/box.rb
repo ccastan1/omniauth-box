e24703c6075f8d23617e799534694bfcbac2b556
Christian Castaneda <christian@bitium.com>
2015-11-05 12:49:27 -0800

    Update box.rb

--- lib/omniauth/strategies/box.rb
+++ lib/omniauth/strategies/box.rb
@@ -7,7 +7,7 @@ module OmniAuth
       option :name, 'box'
       
       option :client_options, {
-        :site => 'https://api.box.com',
+        :site => 'https://app.box.com',
         :authorize_url => 'https://app.box.com/api/oauth2/authorize',
         :token_url     => 'https://app.box.com/api/oauth2/token'
       }
